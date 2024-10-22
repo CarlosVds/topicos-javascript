99ebf01e7e21b8de0a0f0e7822b93d3769a7ff3e
carloseduardovds@gmail.com <carloseduardovds@gmail.com>
2024-10-22 17:17:46 -0300

    manipulating with Math

---
+++ 07-Matematica/manipulacao.js
@@ -0,0 +1,9 @@
+// let novovalor = Math.round(5.44545);
+// let novovalor = Math.round(5.54545);
+// let novovalor = Math.floor(5.4);
+// let novovalor = Math.ceil(5.4);
+// let novovalor = Math.min(19, 2, 200);
+// let novovalor = Math.max(19, 2, 200);
+let novovalor = Math.floor(Math.random() * 100);
+
+console.log(novovalor)
