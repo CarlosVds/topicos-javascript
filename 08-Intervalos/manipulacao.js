4b464d10fca999fbec22ef5d996442cc9b4ac868
carloseduardovds@gmail.com <carloseduardovds@gmail.com>
2024-10-23 16:49:12 -0300

    Working with setTimeout and setInterval

---
+++ 08-Intervalos/manipulacao.js
@@ -0,0 +1,47 @@
+// function showTime(){
+//     let d = new Date();
+
+//     let h = d.getHours();
+//     let m = d.getMinutes();
+//     let s = d.getSeconds();
+
+//     let hrs = h+":"+m+":"+s;
+
+//     document.querySelector('.manipulacao').innerHTML = hrs
+// }
+
+// let timer;
+
+// function comecar(){
+//     timer = setInterval(showTime, 1000);
+// }
+
+// function parar(){
+//     clearInterval(timer)
+// }
+
+let time;
+let frase;
+
+
+function showTime(){
+    let d = new Date();
+
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+
+    hrs = h+":"+m+":"+s; 
+
+    document.querySelector('.texto').innerHTML = "Hora certa :" 
+    document.querySelector('.time').innerHTML = hrs;
+}
+
+function comecar(){
+    time = setInterval(showTime, 1000)
+    frase = setTimeout(showTime,1000)
+}
+
+function parar(){
+    clearTimeout(frase)
+}
