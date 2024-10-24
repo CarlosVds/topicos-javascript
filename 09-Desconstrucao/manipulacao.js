9b3a1d0fa86c5cfe4191525fce7d021691e24b29
carloseduardovds@gmail.com <carloseduardovds@gmail.com>
2024-10-24 17:37:47 -0300

    Descontructing Arrays

--- 09-Desconstrucao/manipulacao.js
+++ 09-Desconstrucao/manipulacao.js
@@ -1,22 +1,34 @@
-let pessoa = {
-    nome: "Carlos",
-    sobreNome: "Santos",
-    idade: 100,
-    social:{
-        facebook: "CarlosVDs",
-        instagram:{
-            url:"@CarlosVds",
-            seguidores: 1000
-        }
-    },
+// let pessoa = {
+//     nome: "Carlos",
+//     sobreNome: "Santos",
+//     idade: 100,
+//     social:{
+//         facebook: "CarlosVDs",
+//         instagram:{
+//             url:"@CarlosVds",
+//             seguidores: 1000
+//         }
+//     },
    
-}
+// }
 
-let {nome, sobreNome, social:{instagram:{url}}} = pessoa;
+// let {nome, sobreNome, social:{instagram:{url}}} = pessoa;
 
-function pegarObjetos(){
-    return `${nome} ${sobreNome} (Siga em ${url})`
-}
+// function pegarObjetos(){
+//     return `${nome} ${sobreNome} (Siga em ${url})`
+// }
 
-console.log(pegarObjetos(pessoa));
-console.log(nome, sobreNome, `(Siga em ${url})`);
+// console.log(pegarObjetos(pessoa));
+// console.log(nome, sobreNome, `(Siga em ${url})`);
+
+let [,,nome,sobreNome ] = ["Carlos Santos", "Marcella", "Enzo","Carvalho"];
+
+console.log(nome, sobreNome);
+
+function criar(){ 
+    return [1,2,3];
+};
+
+let [a,b,c] = criar();
+
+console.log(b)
